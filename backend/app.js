d583566060863276ffed9d3767e90814e07d2835
Meenesh2783 <mr.cool2783@gmail.com>
2024-03-26 15:04:00 +0530

    stage1

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express()
+app.use(express.json())
+const mongoose=require('mongoose')
+const path=require('path')
+const dbPath=path.join(__dirname,)
+module.exports=app
