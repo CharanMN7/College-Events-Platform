c7cb2ad568eb5f843a8cd3217990d8c87dab3fc8
AVidhanR <itsvidhanreddy@gmail.com>
2024-03-27 14:24:35 +0530

    navbar complete
    navbar is completed, @CharanMN7 can you keep the suitable background-color for the navbar, I cannot come up with any

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,7 +1,44 @@
+import raghuLogo from "./raghuLogo.jpg";
+import "./NavBar.scss";
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    // adding event listener
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header>
-      <h2>Raghu Engineering College</h2>
+      <div className="navbar">
+        <img src={raghuLogo} alt="Raghu Enginerring College" />
+        <div className="navbar-menu">
+          <a href="#">About</a>
+          <a href="#">
+            <span className="material-symbols-rounded">account_circle</span>
+          </a>
+        </div>
+        <button onClick={() => setMobileView(!mobileView)}>
+          <span className="material-symbols-rounded">menu</span>
+        </button>
+      </div>
+      {width <= 430 && mobileView && (
+        <div className="navbar-mobile">
+          <a href="#">About</a>
+          <a href="#">Profile</a>
+        </div>
+      )}
     </header>
   );
 };
