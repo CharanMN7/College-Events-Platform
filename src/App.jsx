cd220660815b1219789d629cae228ed1b668aeb6
AVidhanR <itsvidhanreddy@gmail.com>
2024-03-27 15:42:09 +0530

    Completed filter icon
    Completed filter icon display and the labels, tags - the popup style when filter icon was clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Carousel from "./carousel/Carousel";
+import Labels from "./labels/label";
 // import CarouselCard from "./carousel/CarouselCard";
 
 function App() {
-  return <Carousel />;
+  return (
+    <div>
+      <Carousel />
+      <Labels />
+    </div>
+  );
 }
 
 export default App;
