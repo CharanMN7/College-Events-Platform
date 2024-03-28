44efc7144e0e508fd54e886e75be50d44c9a4cad
AVidhanR <itsvidhanreddy@gmail.com>
2024-03-28 10:57:55 +0530

    minor chages in profile.scss
    removed bg color

--- src/user-profile/Profile.jsx
+++ src/user-profile/Profile.jsx
@@ -3,16 +3,21 @@ import bgImg from "../carousel/background.jpg";
 
 const Profile = () => {
   return (
-    <div className="profile">
-      <div>
-        {/** This data should actually come from the [.json] file. For the time being i'm using static info */}
-        <img src={bgImg} width={"130px"} height={"130px"} alt="Profile" />
-        <article className="profile-info">
-          <h1>Student Name</h1>
-          <p>I am a Raghu engg student. I am happy to be here</p>
-        </article>
+    <main>
+      <div className="profile">
+        <div>
+          {/** This data should actually come from the [.json] file. For the time being i'm using static info */}
+          <img src={bgImg} width={"130px"} height={"130px"} alt="Profile" />
+          <article className="profile-info">
+            <h1>Student Name</h1>
+            <p>I am a Raghu engg student. I am happy to be here</p>
+          </article>
+        </div>
       </div>
-    </div>
+      <article>
+        <h1>Events Attended</h1>
+      </article>
+    </main>
   );
 };
 
