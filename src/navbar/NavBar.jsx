37bc227219ac9c354188b4572c547a79616c2db5
CharanMN7_pc <maverickid247@gmail.com>
2024-03-29 17:04:32 +0530

    Removed previously written code to reimagine the NavBar

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,32 +1,8 @@
-import raghuLogo from "./raghuLogo.jpg";
+// import raghuLogo from "./raghuLogo.jpg";
 import "./NavBar.scss";
-import { useState } from "react";
 
 const NavBar = () => {
-  const [mobileView, setMobileView] = useState(false);
-
-  return (
-    <header>
-      <div className="navbar">
-        <img src={raghuLogo} alt="Raghu Enginerring College" />
-        <div className="navbar-menu">
-          <a href="#">About</a>
-          <a href="#">
-            <span className="material-symbols-rounded">account_circle</span>
-          </a>
-        </div>
-        <button onClick={() => setMobileView(!mobileView)}>
-          <span className="material-symbols-rounded">menu</span>
-        </button>
-      </div>
-      {mobileView && (
-        <div className="navbar-mobile">
-          <a href="#">About</a>
-          <a href="#">Profile</a>
-        </div>
-      )}
-    </header>
-  );
+  return <header></header>;
 };
 
 export default NavBar;
