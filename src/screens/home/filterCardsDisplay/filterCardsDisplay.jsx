a021b34f48a01441c43b7d635c53da70b9ea38fb
CharanMN7_pc <maverickid247@gmail.com>
2024-04-01 02:53:53 +0530

    Completed the cards frontend code without functionality

--- src/screens/home/filterCardsDisplay/filterCardsDisplay.jsx
+++ src/screens/home/filterCardsDisplay/filterCardsDisplay.jsx
@@ -1,7 +1,36 @@
 import "./FilterCardsDisplay.scss";
+import { tags, events } from "./test";
+import TheCard from "./TheCard";
 
 const FilterCardsDisplay = () => {
-  return <div></div>;
+  return (
+    <div className="card-display">
+      <h2>Latest Events</h2>
+      <div className="tags">
+        {tags.map((tag) => (
+          <span className="tag tech-tag" key={tag}>
+            {tag}
+          </span>
+        ))}
+        <span className="material-symbols-rounded cancel">cancel</span>
+      </div>
+
+      {/* cards go here */}
+      <div className="cards">
+        {events.map((event) => {
+          return (
+            <TheCard
+              name={event.name}
+              date={event.date}
+              status={event.status}
+              oneLiner={event.oneLiner}
+              key={event.name + event.oneLiner}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default FilterCardsDisplay;
--- src/screens/home/filterCardsDisplay/test.js
+++ src/screens/home/filterCardsDisplay/test.js
@@ -32,4 +32,28 @@ export const events = [
     bgImg: "./background.jpg",
     tags: ["fest", "programming"],
   },
+  {
+    name: "Sparks 2024",
+    date: "April 30, 2024",
+    oneLiner: "Let the sparks of 2024 light up your future!",
+    status: "upcoming",
+    bgImg: "./background.jpg",
+    tags: ["electronics", "fest"],
+  },
+  {
+    name: "GDSC irl2",
+    date: "March 31, 2024",
+    oneLiner: "We have a lot to catch up on, developers, ASSEMBLE!",
+    status: "upcoming",
+    bgImg: "./background.jpg",
+    tags: ["web", "programming"],
+  },
+  {
+    name: "Hackoverflow 2.0",
+    date: "March 26, 2024",
+    oneLiner: "Calling all hackers for a 3 day full-time battle!",
+    status: "live",
+    bgImg: "./background.jpg",
+    tags: ["fest", "programming"],
+  },
 ];
