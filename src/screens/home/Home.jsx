025e299cf3d374550a20d3e1cf887246ac021f99
CharanMN7_pc <maverickid247@gmail.com>
2024-04-01 03:36:57 +0530

    RsvpForm ready

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,14 +1,16 @@
 import Carousel from "./carousel/Carousel";
 import NavBar from "../../reusableComponents/NavBar/NavBar";
 import FilterCardsDisplay from "./filterCardsDisplay/FilterCardsDisplay";
+import RsvpForm from "../../popups/RsvpForm";
 
 const Home = () => {
   return (
-    <>
-      <NavBar />
-      <Carousel />
-      <FilterCardsDisplay />
-    </>
+    // <>
+    //   <NavBar />
+    //   <Carousel />
+    //   <FilterCardsDisplay />
+    // </>
+    <RsvpForm />
   );
 };
 export default Home;
