4dbafdfeb427ba76c63812a29832d974dbace3fc
CharanMN7_pc <maverickid247@gmail.com>
2024-03-31 09:48:50 +0530

    Added NavBar to HomeScreen

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,8 +1,10 @@
 import Carousel from "./carousel/Carousel";
+import NavBar from "../../reusableComponents/NavBar/NavBar";
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       <Carousel />
     </>
   );
