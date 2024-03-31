872f95d41eb7648dd9d461f0302420c7692dd0c9
CharanMN7_pc <maverickid247@gmail.com>
2024-04-01 01:09:03 +0530

    Added filterCardsDisplay to home

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,11 +1,13 @@
 import Carousel from "./carousel/Carousel";
 import NavBar from "../../reusableComponents/NavBar/NavBar";
+import filterCardsDisplay from "./filterCardsDisplay/filterCardsDisplay";
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <Carousel />
+      <filterCardsDisplay />
     </>
   );
 };
