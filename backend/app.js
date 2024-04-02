0455c9a48a5143b8bcad26bfda28cc7a84484870
vara-eugene <vara.eugene9@gmail.com>
2024-04-02 12:37:41 +0530

    admin/all_events api updated
    eventSchema is added to schemas folder and admin/all_events api is added to app.js

--- backend/app.js
+++ backend/app.js
@@ -9,4 +9,34 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.listen(3000);
 
+//this api gets all the events(admin)
+app.get('/api/event',async(req,res)=>{
+    try{
+        const events=await Event.find({});
+        res.status(200).json(events);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+
+});
+//this api gets id-specified event(admin)
+app.get('/api/events/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const event=await Event.findById(id);
+        res.status(200).json(event);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+
+});
+
+
+
+
+
+
+
+
+
 module.exports = app;
