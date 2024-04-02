8230630b5bdcb8227e3e8dca511a3e09c905c594
vara-eugene <vara.eugene9@gmail.com>
2024-04-02 13:29:26 +0530

    updated create-event (admin) api
    create-event api is created and added to app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.listen(3000);
 
-//this api gets all the events(admin)
+//this api gets all the events from db(admin)
 app.get('/api/event',async(req,res)=>{
     try{
         const events=await Event.find({});
@@ -19,7 +19,7 @@ app.get('/api/event',async(req,res)=>{
     }
 
 });
-//this api gets id-specified event(admin)
+//this api gets id-specified event from db(admin)
 app.get('/api/events/:id',async(req,res)=>{
     try{
         const {id}=req.params;
@@ -31,8 +31,19 @@ app.get('/api/events/:id',async(req,res)=>{
 
 });
 
+//this api creates an event(admin)
+app.post('/api/events',async (req,res)=>{
+    //console.log(req.body);
+    //res.send(req.body);
+    try{
+        const event=await Event.create(req.body);
+        res.status(200).json(event);
 
-
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+);
 
 
 
