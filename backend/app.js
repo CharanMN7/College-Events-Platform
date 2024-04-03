4336e40b4f9f17f107f93b15d6c5d702c0b096a7
AVidhanR <itsvidhanreddy@gmail.com>
2024-04-03 21:51:57 +0530

    indentation check

--- backend/app.js
+++ backend/app.js
@@ -7,47 +7,50 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
 
+/**
+ * in ES version
+ * import express, { json } from "express";
+import { connect } from "mongoose";
+const dotenv = require("dotenv").config();
+
+const app = express();
+app.use(json());
+
+connect(process.env.MONGO_URI);
+ */
+
 app.listen(3000);
 
 //this api gets all the events from db(admin)
-app.get('/api/events',async(req,res)=>{
-    try{
-        const event=await Event.find({});
-        res.status(200).json(event);
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
-
+app.get("/api/events", async (req, res) => {
+  try {
+    const event = await Event.find({});
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 //this api gets id-specified event from db(admin)
-app.get('/api/events/:id',async(req,res)=>{
-    try{
-        const {id}=req.params;
-        const event=await Event.findById(id);
-        res.status(200).json(event);
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
-
+app.get("/api/events/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.findById(id);
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //this api creates an event(admin)
-app.post('/api/events',async (req,res)=>{
-    //console.log(req.body);
-    //res.send(req.body);
-    try{
-        const event=await Event.create(req.body);
-        res.status(200).json(event);
-
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
-}
-);
-
-
-
-
-
+app.post("/api/events", async (req, res) => {
+  //console.log(req.body);
+  //res.send(req.body);
+  try {
+    const event = await Event.create(req.body);
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = app;
