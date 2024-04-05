09f4010134299ec1d4c42e1384e09a6fd2fb9f05
AVidhanR <itsvidhanreddy@gmail.com>
2024-04-05 09:42:58 +0530

    commented the update in index

--- backend/allevents-api/index.js
+++ backend/allevents-api/index.js
@@ -8,19 +8,19 @@ const app = express();
 app.use(express.json());
 
 //update a product
-app.put("/api/events/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const event = await Event.findByIdAndUpdate(id, req.body);
-    if (!event) {
-      return res.status(404).json({ message: "event not found" });
-    }
-    const updatedevent = await Event.findById(id);
-    res.status(200).json(updatedevent);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// app.put("/api/events/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const event = await Event.findByIdAndUpdate(id, req.body);
+//     if (!event) {
+//       return res.status(404).json({ message: "event not found" });
+//     }
+//     const updatedevent = await Event.findById(id);
+//     res.status(200).json(updatedevent);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 //delete a product
 app.delete("/api/events/:id", async (req, res) => {
   try {
