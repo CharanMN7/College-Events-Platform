35fbc2ece4a4bd3610c0f30f96d81b3488586759
AVidhanR <itsvidhanreddy@gmail.com>
2024-04-05 09:30:29 +0530

    commented the app.post() in index
    reason - already in app.js

--- backend/allevents-api/index.js
+++ backend/allevents-api/index.js
@@ -36,16 +36,16 @@ app.use(express.json());
 //   }
 // });
 
-app.post("/api/events", async (req, res) => {
-  //console.log(req.body);
-  //res.send(req.body);
-  try {
-    const event = await Event.create(req.body);
-    res.status(200).json(event);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// app.post("/api/events", async (req, res) => {
+//   //console.log(req.body);
+//   //res.send(req.body);
+//   try {
+//     const event = await Event.create(req.body);
+//     res.status(200).json(event);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 mongoose
   .connect(
