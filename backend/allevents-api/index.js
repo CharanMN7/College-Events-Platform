3f46081181a176c97e071a84ace58334ffa0b1d5
AVidhanR <itsvidhanreddy@gmail.com>
2024-04-05 09:50:47 +0530

    delete index.js

--- backend/allevents-api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-// const Event = require("./eventSchema.js");
-//const dotenv = require("dotenv").config();
-
-const app = express();
-//middleware
-app.use(express.json());
