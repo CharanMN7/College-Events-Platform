49d21d55bd3815ea4de0f7b9bacfc6829916348c
vara-eugene <vara.eugene9@gmail.com>
2024-04-07 21:39:47 +0530

    commit
    db connection resolved

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
-const express = require("express");
+/*const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const Event=require('../eventSchema.js');
 
 const app = express();
 app.use(express.json());
@@ -45,9 +46,95 @@ app.post('/api/events',async (req,res)=>{
 }
 );
 
+module.exports = app;*/
+
+const express=require('express')
+const mongoose=require('mongoose')
+const Event=require('./schemas/eventSchema.js');
+//const dotenv = require("dotenv").config();
+
+const app=express()
+//middleware
+app.use(express.json());
+app.listen(3000,()=>{
+    console.log('server running');
+});
+app.get('/',(req,res)=>{
+res.send("hello from node api");
+});
+//this api gets all events from the database(admin)
+app.get('/api/events',async(req,res)=>{
+    try{
+        const event=await Event.find({});
+        res.status(200).json(event);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+
+});
+//this api gets id-specified events from database(admin)
+app.get('/api/events/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const event=await Event.findById(id);
+        res.status(200).json(event);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+
+});
+//this api creates an event
+app.post('/api/events',async (req,res)=>{
+    //console.log(req.body);
+    //res.send(req.body);
+    try{
+        const event=await Event.create(req.body);
+        res.status(200).json(event);
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+);
+//db connection
+mongoose.connect("mongodb+srv://charan:cherrythebest@incrediblemango.ha5o1tn.mongodb.net/?retryWrites=true&w=majority&appName=IncredibleMango")
+.then(()=>{
+    console.log('connected to db');
+})
+.catch(()=>{
+    console.log('connection failed');
+})
 
 
+//this api updates an event
+app.put('/api/events/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const event=await Event.findByIdAndUpdate(id,req.body);
+        if(!event){
+            return res.status(404).json({message: "event not found"});
+        }
+        const updatedevent=await Event.findById(id);
+        res.status(200).json(updatedevent);
 
 
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
 
-module.exports = app;
+});
+//this api deletes an event
+app.delete('/api/events/:id',async(req,res)=>{
+    try{
+        const {id} =req.params;
+        const event=await Event.findByIdAndDelete(id);
+        if(!event){
+            return res.status(404).json({message:"event not found"});
+        }
+        res.status(200).json({message:"event deleted successfully"});
+
+    }catch(error)
+    {
+        res.status(500).json({message:error.message});
+    }
+})
