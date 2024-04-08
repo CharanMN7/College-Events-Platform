0a6a45d91777fe5c18b6b6b3405c88aee7073071
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 11:07:20 +0530

    /event/:id API added. Tested and Working.

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,12 @@ app.get("/all-events", async (req, res) => {
 });
 
 // returns details of all events without attendees info
-app.get("/event/:id", async (req, res) => {});
+app.get("/event/:id", async (req, res) => {
+  const eventId = req.params.id;
+  const theEvent = await Event.findOne({ _id: eventId });
+
+  res.send(theEvent);
+});
 
 // saves attendee info to the event (puts the rsvp info into the attendees field of the event)
 app.put("/event/:id/rsvp", async (req, res) => {});
