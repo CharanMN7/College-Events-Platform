44f8558b4066edd0644b1ad38923328f64d06781
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 20:40:01 +0530

    Updated Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,28 @@ import EventPage from "./screens/eventPage/EventPage";
 import Dashboard from "./screens/admin/Dashboard";
 import Home from "./screens/home/Home";
 import CreateEvent from "./screens/theEvent/CreateEvent";
+import Login from "./forms/Login";
+import ViewEvent from "./screens/admin/ViewEvent";
+import { useState } from "react";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/event" element={<EventPage />} />
-        <Route path="/admin" element={<Dashboard />} />
-        <Route
-          path="/create-event"
-          element={<CreateEvent operation="Create" />}
-        />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event/:id" element={<EventPage />} />
+          <Route
+            path="/admin"
+            element={isLoggedIn ? <Dashboard /> : <Login />}
+          />
+          <Route
+            path="/admin/create-event"
+            element={
+              isLoggedIn ? <CreateEvent operation="create" /> : <Login />
+            }
+          />
+          <Route path="/admin/event/:id" element={<ViewEvent />} />
+        </Routes>
     </BrowserRouter>
   );
 }
