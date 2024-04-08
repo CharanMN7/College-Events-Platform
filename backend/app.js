5432333cb00a52d8e39a015aebf7fe5f4dee3d2f
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 06:23:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,55 +1,55 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
-const app=express()
-const Login=require('./schemas/loginSchema')
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const Login = require("./schemas/loginSchema");
+
 //middleware
 app.use(express.json());
 //app.use(express.urlencoded({encoded:false}));
 //routes
 //app.use("/api/products",productRoute);
 
-
-
-
-
-app.listen(3000,()=>{
-    console.log('server running');
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/api/login',async (req,res)=>{
-    try{
-        const {username, password} = req.body
-        const dbUser=await Login.findOne({username})
-        if(!dbUser){
-            return res.status(400).json({message:'user not found'});
-        }
-        const isPasswordMatched= await dbUser.password === password
-        if(isPasswordMatched ){
-            const token = jwt.sign({ username: username }, "zxcvbnmasdfghjkl");
-            return res.json({ message: 'Login successful', token });
-        }else{
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const dbUser = await Login.findOne({ username });
 
-    }catch(error){
-        res.send(error)
+    if (!dbUser) {
+      return res.status(400).json({ message: "user not found" });
     }
-}
-);
 
-  
+    const isPasswordMatched = (await dbUser.password) === password;
 
+    if (isPasswordMatched) {
+      const token = jwt.sign({ username: username }, "zxcvbnmasdfghjkl");
+      return res.json({ message: "Login successful", token });
+    } else {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-mongoose.connect("mongodb+srv://charan:cherrythebest@incrediblemango.ha5o1tn.mongodb.net/?retryWrites=true&w=majority&appName=IncredibleMango")
-.then(()=>{
-    console.log('connected to db');
-})
-.catch(()=>{
-    console.log('connection failed');
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
 
-
-//update a product
-
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
