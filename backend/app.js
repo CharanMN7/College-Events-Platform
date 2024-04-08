e27af2c9511c5ab90adf97d4ed512e491cd55303
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 11:58:59 +0530

    Added the missing slashes to API routes

--- backend/app.js
+++ backend/app.js
@@ -52,19 +52,35 @@ app.put("/event/:id/rsvp", async (req, res) => {
 // Admin APIs
 
 // gets details of all events including attendees info
-app.get("admin/all-events", async (req, res) => {});
+app.get("/admin/all-events", async (req, res) => {});
 
 // gets details of all event including attendees info
-app.get("admin/event/:id", async (req, res) => {});
+app.get("/admin/event/:id", async (req, res) => {});
 
 // saves new event data to the events collection
-app.put("admin/create-event", async (req, res) => {});
+app.put("/admin/create-event", async (req, res) => {});
 
 // updates the details of an existing event
-app.post("admin/event/:id/update", async (req, res) => {});
+app.post("/admin/event/:id/update", async (req, res) => {});
 
 // gets the jwt token upon successful admin user verification (not robust)
-app.get("/admin/login", async (req, res) => {});
+app.get("/admin/login", async (req, res) => {
+  const { username, password } = req.body;
+  const dbUser = await Admin.findOne({ username: username });
+
+  if (!dbUser) {
+    return res.status(400).json({ message: "user not found" });
+  }
+
+  const isPasswordMatched = (await dbUser.password) === password;
+
+  if (isPasswordMatched) {
+    const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
+    return res.json({ message: "Login Successful", token });
+  } else {
+    return res.status(401).json({ message: "Wrong Password" });
+  }
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
