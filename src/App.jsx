5edb6ffce5bad15ba857485df730a0664dd67957
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 22:20:13 +0530

    Setup context to use React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./screens/home/Home";
+import Login from "./forms/Login";
 import EventPage from "./screens/eventPage/EventPage";
 import Dashboard from "./screens/admin/Dashboard";
-import Home from "./screens/home/Home";
-import CreateEvent from "./screens/theEvent/CreateEvent";
-import Login from "./forms/Login";
 import ViewEvent from "./screens/admin/ViewEvent";
-import { useState } from "react";
+import CreateEvent from "./screens/theEvent/CreateEvent";
 import LoginContext from "./utils/LoginContext";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState("");
+
+  const queryClient = new QueryClient({
+    queries: {
+      cacheTime: Infinity,
+      staleTime: Infinity,
+    },
+  });
 
   return (
     <BrowserRouter>
@@ -20,34 +28,36 @@ function App() {
           theToken: [token, setToken],
         }}
       >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/event/:id" element={<EventPage />} />
-          <Route
-            path="/admin"
-            element={isLoggedIn ? <Dashboard /> : <Login />}
-          />
-          <Route
-            path="/admin/create-event"
-            element={
-              isLoggedIn ? <CreateEvent operation="Create" /> : <Login />
-            }
-          />
-          <Route path="/admin/event/:id" element={<ViewEvent />} />
-          {/*
-           **************
-           * AllEvents Route:
-           * <Route path="/all-events" element={<AllEvents />} />
-           *
-           * update event route:
-           * <Route path="/admin/event/:id/update"
-           *   element={
-           *     isLoggedIn ? <CreateEvent operation="Update" /> : <Login />
-           *   }
-           * />
-           * ************
-           */}
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/event/:id" element={<EventPage />} />
+            <Route
+              path="/admin"
+              element={isLoggedIn ? <Dashboard /> : <Login />}
+            />
+            <Route
+              path="/admin/create-event"
+              element={
+                isLoggedIn ? <CreateEvent operation="Create" /> : <Login />
+              }
+            />
+            <Route path="/admin/event/:id" element={<ViewEvent />} />
+            {/*
+             **************
+             * AllEvents Route:
+             * <Route path="/all-events" element={<AllEvents />} />
+             *
+             * update event route:
+             * <Route path="/admin/event/:id/update"
+             *   element={
+             *     isLoggedIn ? <CreateEvent operation="Update" /> : <Login />
+             *   }
+             * />
+             * ************
+             */}
+          </Routes>
+        </QueryClientProvider>
       </LoginContext.Provider>
     </BrowserRouter>
   );
