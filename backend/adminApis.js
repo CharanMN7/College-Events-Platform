9c137eb99ad2442851d3251b4cc11b8e5e914b54
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 07:59:01 +0530

    Defined required admin APIs

--- backend/adminApis.js
+++ backend/adminApis.js
@@ -1,1 +1,11 @@
 import app from "./app";
+
+app.get("admin/all-events", async (req, res) => {});
+
+app.get("admin/event/:id", async (req, res) => {});
+
+app.put("admin/create-event", async (req, res) => {});
+
+app.post("admin/event/:id/update", async (req, res) => {});
+
+app.get("admin/login", async (req, res) => {});
