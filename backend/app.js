b016a97ea27108a022c23f6af37247447d1ecd1b
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 11:40:09 +0530

    Added admin APIs to app.js

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,22 @@ app.put("/event/:id/rsvp", async (req, res) => {
   res.send("Done!");
 });
 
+// Admin APIs
+
+// gets details of all events including attendees info
+app.get("admin/all-events", async (req, res) => {});
+
+// gets details of all event including attendees info
+app.get("admin/event/:id", async (req, res) => {});
+
+// saves new event data to the events collection
+app.put("admin/create-event", async (req, res) => {});
+
+// updates the details of an existing event
+app.post("admin/event/:id/update", async (req, res) => {});
+
+// gets the jwt token upon successful admin user verification (not robust)
+app.get("/admin/login", async (req, res) => {});
 
 mongoose
   .connect(process.env.MONGO_URI)
