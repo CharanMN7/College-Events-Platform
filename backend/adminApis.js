dd5fb5442dd42e4c3a9a9fce6afb82aa65af566d
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 08:01:43 +0530

    Added useful comments for the admin APIs

--- backend/adminApis.js
+++ backend/adminApis.js
@@ -1,11 +1,16 @@
 import app from "./app";
 
+// gets details of all events including attendees info
 app.get("admin/all-events", async (req, res) => {});
 
+// gets details of all event including attendees info
 app.get("admin/event/:id", async (req, res) => {});
 
+// saves new event data to the events collection
 app.put("admin/create-event", async (req, res) => {});
 
+// updates the details of an existing event
 app.post("admin/event/:id/update", async (req, res) => {});
 
+// gets the jwt token upon successful admin user verification (not robust)
 app.get("admin/login", async (req, res) => {});
