4c023d1a94bd739fb292b9b7914037087d6e9e9f
CharanMN7_pc <maverickid247@gmail.com>
2024-04-08 20:40:28 +0530

    Created LoginContext to monitor login in all admin screens

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ import CreateEvent from "./screens/theEvent/CreateEvent";
 import Login from "./forms/Login";
 import ViewEvent from "./screens/admin/ViewEvent";
 import { useState } from "react";
+import LoginContext from "./utils/LoginContext";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
+
   return (
     <BrowserRouter>
+      <LoginContext.Provider
+        value={{
+          loginStatus: [isLoggedIn, setIsLoggedIn],
+          theToken: [token, setToken],
+        }}
+      >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/event/:id" element={<EventPage />} />
@@ -25,6 +35,7 @@ function App() {
           />
           <Route path="/admin/event/:id" element={<ViewEvent />} />
         </Routes>
+      </LoginContext.Provider>
     </BrowserRouter>
   );
 }
