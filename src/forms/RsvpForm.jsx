3fd4e73e72743d7d9034672f26254e54b6089ea4
CharanMN7_pc <maverickid247@gmail.com>
2024-04-09 20:53:52 +0530

    Connected RSVP with backend!

--- src/forms/RsvpForm.jsx
+++ src/forms/RsvpForm.jsx
@@ -1,10 +1,34 @@
 /* eslint-disable react/prop-types */
 import "./forms.scss";
+import axios from "axios";
 
-const RsvpForm = ({ close, doClose }) => {
+const RsvpForm = ({ close, doClose, id }) => {
   return (
     <div className="rsvp-container">
-      <form className="rsvp-form">
+      <form
+        className="rsvp-form"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          // form data logic goes here
+          const formData = new FormData(e.target);
+          const data = {
+            fname: formData.get("fname"),
+            lname: formData.get("lname"),
+            email: formData.get("email"),
+          };
+
+          // api fetching logic goes here.
+          const baseUrl = "https://raghu-clubs.onrender.com";
+          const apiRes = await axios.put(baseUrl + `/event/${id}/rsvp`, data);
+
+          if (apiRes.status) {
+            alert("You have RSVP'd successfully! ✅");
+            doClose(!close);
+          } else {
+            alert("Oops, couldn't RSVP at the moment. Please try again.");
+          }
+        }}
+      >
         <span
           className="material-symbols-rounded"
           onClick={() => {
@@ -16,11 +40,29 @@ const RsvpForm = ({ close, doClose }) => {
 
         <p>RSVP for the event</p>
 
-        <input type="text" name="fname" id="" placeholder="First Name" />
+        <input
+          type="text"
+          name="fname"
+          id="fname"
+          placeholder="First Name"
+          required
+        />
 
-        <input type="text" name="lname" id="" placeholder="Last Name" />
+        <input
+          type="text"
+          name="lname"
+          id="lname"
+          placeholder="Last Name"
+          required
+        />
 
-        <input type="email" name="email" id="" placeholder="Email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          required
+        />
 
         <button>RSVP</button>
       </form>
